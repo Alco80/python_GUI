1d12cdd3d6fdc52dfb4a7500684dacdfba5602a2
Alco80 <leeshla2525@gmail.com>
2024-12-14 22:54:12 -0500

    Update project.py

--- project.py
+++ project.py
@@ -9,6 +9,8 @@ def on_click():
 lbl = tk.Label(root, text="This Label")
 lbl.grid(row=0, column=0)
 
+print(lbl.config().keys())
+
 btn = tk.Button(root, text="Press Me!", command=on_click)
 btn.grid(row=0, column=1)
 
