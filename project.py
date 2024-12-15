d110a00edad684aa5ebf5ce3fc47e6efeef696d6
Alco80 <leeshla2525@gmail.com>
2024-12-14 22:47:37 -0500

    Update project.py

--- project.py
+++ project.py
@@ -3,5 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("My App")
 
+lbl = tk.Label(root, text="This Label")
+lbl.grid()
+
 btn = tk.Button(root, text="Press Me!")
+btn.grid()
+
 root.mainloop()
