fa882fd5ebc2e31257ba6666afc6567c2e472a63
Alco80 <leeshla2525@gmail.com>
2024-12-14 22:55:50 -0500

    Update project.py

--- project.py
+++ project.py
@@ -3,15 +3,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title("My App")
 
-def on_click():
-    lbl.config(text="Button click")
-
-lbl = tk.Label(root, text="This Label")
-lbl.grid(row=0, column=0)
-
-print(lbl.config().keys())
-
-btn = tk.Button(root, text="Press Me!", command=on_click)
-btn.grid(row=0, column=1)
+frame = tk.Frame(root)
+frame.grid(row=0, column=0)
 
 root.mainloop()
