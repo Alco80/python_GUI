ec51af512cfa081e84f7a344deddca83969aa3a8
Alco80 <leeshla2525@gmail.com>
2024-12-14 22:58:14 -0500

    Update project.py

--- project.py
+++ project.py
@@ -6,4 +6,10 @@ root.title("My App")
 frame = tk.Frame(root)
 frame.grid(row=0, column=0)
 
+entry = tk.Entry(frame)
+entry.grid(row=0, column=0)
+
+entry_btn = tk.Button(frame, text="Add")
+entry_btn.grid(row=0, column=0)
+
 root.mainloop()
